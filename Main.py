209d4b680c1e72af1f90a05330867f4cc6783493
zesutton2619 <93284379+zesutton2619@users.noreply.github.com>
2021-12-01 13:20:29 -0500

    Add files via upload

--- Main.py
+++ Main.py
@@ -2,13 +2,32 @@
 # A demonstration of different python operations
 
 
-def cont(yes_no):
+def cont():
+    run_cont = True
     cont_run = True
-    if yes_no == "yes":
-        cont_run = False
-    elif yes_no == "no":
-        cont_run = True
+    while run_cont:
+        yes_no = input()
+        if yes_no == "yes" or yes_no == "y":
+            cont_run = False
+            run_cont = False
+        elif yes_no == "no" or yes_no == "n":
+            cont_run = True
+            run_cont = False
+        else:
+            print("Please enter \"yes\" or \"no\":")
     return cont_run
+
+
+def integer_input():
+    execute = True
+    integer = 0
+    while execute:
+        try:
+            integer = int(input())
+            execute = False
+        except ValueError:
+            print("Please enter an integer:")
+    return integer
 
 
 run = False
@@ -36,65 +55,81 @@ while not (run):
         "\nThis next example you will enter a word and a number. Python will t"
         "hen print that word however many times you tell it to.")
     str2 = input("Enter a word: ")
-    num0 = int(input(
-        "Enter how many times you want to print this word in succession: "))
+    print("Enter how many times you want to print this word in succession: ")
+    num0 = integer_input()
     # obtains input from user and converts into integer
     print(str2 * num0)
 
     print(
         "\nThis next example you will enter two numbers and python will add th"
         "em together using the \"+\" operator.")
-    num1 = int(input("Enter a number that is an integer: "))
-    num2 = int(input("Enter another number that is an integer: "))
+    print("Enter a number that is an integer: ")
+    num1 = integer_input()
+    print("Enter another number that is an integer: ")
+    num2 = integer_input()
     print(num1, "+", num2, "=", num1 + num2)
 
     print(
         "\nThe next example will be using the \"-\" operator to subtract the t"
         "wo numbers.")
-    num3 = int(input("Enter a number that is an integer: "))
-    num4 = int(input("Enter another number that is an integer: "))
+    print("Enter a number that is an integer: ")
+    num3 = integer_input()
+    print("Enter another number that is an integer: ")
+    num4 = integer_input()
     print(num3, "-", num4, "=", num3 - num4)
 
     print(
         "\nThe next example will be using the \"/\" operator to divide two num"
         "bers.")
-    div1 = int(input("Enter a number that is an integer: "))
-    div2 = int(input("Enter another number that is an integer: "))
+    print("Enter a number that is an integer: ")
+    div1 = integer_input()
+    print("Enter another number that is an integer: ")
+    div2 = integer_input()
     print(div1, "/", div2, "=", float(div1 / div2))
 
     print(
         "\nThe next example will be using the ""%" + " modulus operator to ret"
-        "urn the remainder after dividing two numbers.")
-    mod1 = int(input("Enter a number that is an integer: "))
-    mod2 = int(input("Enter another number that is an integer: "))
+                                                     "urn the remainder after dividing two numbers.")
+    print("Enter a number that is an integer: ")
+    mod1 = integer_input()
+    print("Enter another number that is an integer: ")
+    mod2 = integer_input()
     print(mod1, "%", mod2, "=", mod1 % mod2)
 
     print(
         "\nThe next example will be using the \"//\" operator which divides tw"
         "o numbers using floor division.")
-    floor_div1 = int(input("Enter a number that is an integer: "))
-    floor_div2 = int(input("Enter another number that is an integer: "))
+    print("Enter a number that is an integer: ")
+    floor_div1 = integer_input()
+    print("Enter another number that is an integer: ")
+    floor_div2 = integer_input()
     print(floor_div1, "//", floor_div2, "=", floor_div1 // floor_div2)
 
     print(
         "\nThe next example will use the \"*\" operator to multiply two number"
         "s together.")
-    mult1 = int(input("Enter a number that is an integer: "))
-    mult2 = int(input("Enter another number that is an integer: "))
+    print("Enter a number that is an integer: ")
+    mult1 = integer_input()
+    print("Enter another number that is an integer: ")
+    mult2 = integer_input()
     print(mult1, "*", mult2, "=", mult1 * mult2)
 
     print(
         "\nThis final example will use the \"**\" operator to take a number to"
         " an exponent.")
-    num5 = int(input("Enter a number that is an integer: "))
-    exp = int(input("Enter an integer for an exponent: "))
+    print("Enter a number that is an integer: ")
+    num5 = integer_input()
+    print("Enter an integer for an exponent: ")
+    exp = integer_input()
     print(num5, "**", exp, "=", num5 ** exp)
 
     print(
         "\nThe next example will look at the boolean operator \"==\" that comp"
         "ares two values to see if they are equal.")
-    num6 = int(input("Enter a number that is an integer: "))
-    num7 = int(input("Enter another number that is an integer: "))
+    print("Enter a number that is an integer: ")
+    num6 = integer_input()
+    print("Enter another number that is an integer: ")
+    num7 = integer_input()
     if num6 == num7:
         print("The numbers are equal")
     else:
@@ -103,8 +138,10 @@ while not (run):
     print(
         "\nThe next example will look at another boolean operator \"!=\" that "
         "compares to values to see if they are not equal.")
-    num8 = int(input("Enter a number that is an integer: "))
-    num9 = int(input("Enter another number that is an integer: "))
+    print("Enter a number that is an integer: ")
+    num8 = integer_input()
+    print("Enter another number that is an integer: ")
+    num9 = integer_input()
     if num8 != num9:
         print("The numbers are not equal")
     else:
@@ -113,15 +150,17 @@ while not (run):
     print(
         "\nThe next example will look at another boolean operator \">\" to com"
         "pare if one number is greater than another")
-    num10 = int(input("Enter a number that is an integer: "))
-    num11 = int(input("Enter another number that is an integer: "))
+    print("Enter a number that is an integer: ")
+    num10 = integer_input()
+    print("Enter another number that is an integer: ")
+    num11 = integer_input()
     if num10 > num11 and num11 < num10:
         print(num10, "is greater than", num11)
     elif num10 == num11 or num10 < num11:
         print(num10, "is less than or equal to", num11)
-    answer = input(
+    print(
         "If you would like to use the demonstration again then enter \"yes\" i"
-        "f not then enter \"no\"")
-    run = cont(answer)
+        "f not then enter \"no\": ")
+    run = cont()
 for end in range(5):
     print("Thank you for using my demonstration!")
